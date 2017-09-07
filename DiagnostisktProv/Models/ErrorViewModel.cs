2a7a5ac27d91eff41f5021b344dd2da18addfc70
sTaMiNa01 <Matilda.Lind@yh.nackademin.se>
2017-09-07 09:43:46 +0200

    Add project files.

---
+++ DiagnostisktProv/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DiagnostisktProv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
