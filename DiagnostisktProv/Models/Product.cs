018187fb3720afab59a3d453cf4d43a388917036
sTaMiNa01 <Matilda.Lind@yh.nackademin.se>
2017-09-07 10:06:58 +0200

    added range and required

--- DiagnostisktProv/Models/Product.cs
+++ DiagnostisktProv/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace DiagnostisktProv.Models
     public class Product
     {
         public Guid ProductID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(5,1000)]
         public Decimal Price { get; set; }
     }
 }
